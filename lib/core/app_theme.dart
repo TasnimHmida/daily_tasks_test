74da17a9bfc46a6f03b06cda6317b0ba52359f83
Tasnim Hmida <tasnimhmida@takiacademyteam.com>
2024-05-08 11:13:55 +0100

    added core files

---
+++ lib/core/app_theme.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const goldenRod = Color(0xFFFED36A);
+const ebonyClay = Color(0xFF212832);
---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,3 @@
+const BASE_URL = "https://www.savaneshop.com";
+// const BASE_URL = "https://savane.preprod.l-wa.com";
+// const BASE_URL = "https://www.pac.savane.laxmi.cloud";
